d104975a3e3e0a470d585fb3408cee4931b5c4ac
SunChan07 <mix96.08.12@gmail.com>
2026-03-29 09:29:10 +0900

    Update GhostThemeSystem.cs

--- Content.Client/_Sunrise/GhostTheme/GhostThemeSystem.cs
+++ Content.Client/_Sunrise/GhostTheme/GhostThemeSystem.cs
@@ -1,6 +1,7 @@
 // © SUNRISE, An EULA/CLA with a hosting restriction, full text: https://github.com/space-sunrise/space-station-14/blob/master/CLA.txt
 using Content.Shared._Sunrise.GhostTheme;
-using Content.Shared.Weapons.Ranged.Systems;
+using Content.Shared.DrawDepth;
+using Content.Shared.Weapons.Ranged.Systems; // EffectLayers
 using Robust.Client.GameObjects;
 using Robust.Shared.Prototypes;
 
@@ -8,32 +9,33 @@ namespace Content.Client._Sunrise.GhostTheme;
 
 public sealed class GhostThemeSystem : EntitySystem
 {
-[Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
     public override void Initialize()
-{
-    base.Initialize();
-    SubscribeLocalEvent<GhostThemeComponent, AfterAutoHandleStateEvent>(OnInit);
-}
+    {
+        base.Initialize();
+        SubscribeLocalEvent<GhostThemeComponent, AfterAutoHandleStateEvent>(OnInit);
+    }
 
-private void OnInit(EntityUid uid, GhostThemeComponent component, ref AfterAutoHandleStateEvent args)
-{
-    if (component.GhostTheme == null
-        || !_prototypeManager.TryIndex<GhostThemePrototype>(component.GhostTheme, out var ghostThemePrototype))
-        return;
+    private void OnInit(EntityUid uid, GhostThemeComponent component, ref AfterAutoHandleStateEvent args)
+    {
+        if (component.GhostTheme == null
+            || !_prototypeManager.TryIndex<GhostThemePrototype>(component.GhostTheme, out var ghostThemePrototype))
+            return;
 
-    if (!EntityManager.TryGetComponent<SpriteComponent>(uid, out var sprite))
-        return;
+        if (!TryComp(uid, out SpriteComponent sprite))
+            return;
+        
+        if (!sprite.LayerMapTryGet(EffectLayers.Unshaded, out var layer))
+            layer = sprite.LayerMapReserveBlank(EffectLayers.Unshaded);
 
-    if (!sprite.LayerMapTryGet(EffectLayers.Unshaded, out var layer))
-        layer = sprite.LayerMapReserveBlank(EffectLayers.Unshaded);
-
-    sprite.LayerSetSprite(layer, ghostThemePrototype.Sprite);
-    sprite.LayerSetShader(layer, "unshaded");
-    sprite.LayerSetColor(layer, ghostThemePrototype.SpriteColor);
-    sprite.LayerSetScale(layer, ghostThemePrototype.Scale);
-
-    sprite.DrawDepth = DrawDepth.Default + 13;
-    sprite.OverrideContainerOcclusion = true;
-    sprite.NoRotation = true;
+        sprite.LayerSetSprite(layer, ghostThemePrototype.Sprite);
+        sprite.LayerSetShader(layer, "unshaded");
+        sprite.LayerSetColor(layer, ghostThemePrototype.SpriteColor);
+        sprite.LayerSetScale(layer, ghostThemePrototype.Scale);
+        
+        sprite.DrawDepth = DrawDepth.Default + 13;
+        sprite.OverrideContainerOcclusion = true;
+        sprite.NoRotation = true;
+    }
 }
