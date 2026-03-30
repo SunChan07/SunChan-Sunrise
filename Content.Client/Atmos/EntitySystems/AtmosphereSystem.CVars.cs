b8f7eeb0b33fff66c26d90ad980cf8a99b0622e9
SunChan07 <mix96.08.12@gmail.com>
2026-03-31 01:35:15 +0900

    True/False

--- Content.Client/Atmos/EntitySystems/AtmosphereSystem.CVars.cs
+++ Content.Client/Atmos/EntitySystems/AtmosphereSystem.CVars.cs
@@ -6,10 +6,11 @@ namespace Content.Client.Atmos.EntitySystems;
 public sealed partial class AtmosphereSystem
 {
     [Dependency] private readonly IConfigurationManager _cfg = default!;
-    private readonly Subscriptions _subs = new();
+    private EntitySystem.Subscriptions _subs; // Sunrise edit
 
     private void InitializeCVars()
     {
+        _subs = new(this);
         _subs.CVar(_cfg, CCVars.AtmosHeatScale,
             v => HeatScale = MathF.Max(0.000001f, v),
             invokeImmediately: true);
@@ -17,7 +18,7 @@ public sealed partial class AtmosphereSystem
 
     public override void Shutdown()
     {
-        _subs.Dispose();
+        _subs.Cleanup();
         base.Shutdown();
     }
 }
