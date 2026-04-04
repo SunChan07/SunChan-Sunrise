6f707eda58d821fdb75fac49f9dce3fba19c1dff
SunChan07 <mix96.08.12@gmail.com>
2026-04-04 23:29:55 +0900

    Ага, дальше

--- Content.Client/_Sunrise/Sandbox/SandboxUIController.cs
+++ Content.Client/_Sunrise/Sandbox/SandboxUIController.cs
@@ -1,14 +1,6 @@
-﻿using System.Linq;
-using Content.Server.GameTicking;
-using Content.Shared.Sandbox;
-using Robust.Server.Player;
-using Robust.Shared.Enums;
-using Robust.Shared.Player;
-using Robust.Shared.Prototypes;
-using Content.Shared._Sunrise.ThermalVision;
-using Content.Shared._Sunrise.Sandbox;
+﻿using Content.Client.Sandbox;
 
-namespace Content.Server.Sandbox
+namespace Content.Client.UserInterface.Systems.Sandbox;
 
 public partial class SandboxUIController : UIController, IOnStateChanged<GameplayState>, IOnSystemChanged<SandboxSystem>
 {
